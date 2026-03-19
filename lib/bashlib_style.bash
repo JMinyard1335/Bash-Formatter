62d0bc69f2bc875bb3978f45366922826f44ed17
Jachin Minyard <jachinminyard@gmail.com>
2026-03-19 13:26:28 -0500

    added lib file for sourcing

---
+++ lib/bashlib_style.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Source file for all the lib functions
+
+if  [[ -v bashlib_style_sourced ]]; then
+    return 0
+fi
+bashlib_style_sourced=1
+
+bashlib_style_source_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+source "${bashlib_style_source_dir}/internal/bashlib_print.bash"
+source "${bashlib_style_source_dir}/internal/bashlib_menu.bash"
